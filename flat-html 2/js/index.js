1f70fb564152de1e0c868f210bded3dd90196fe5
Adrienne Polk <apolk7@gmail.com>
2016-04-02 14:29:04 -0400

    Final Project 4.2.16

--- flat-html 2/js/index.js
+++ flat-html 2/js/index.js
@@ -1,10 +1,10 @@
 //wait for the DOM elements to load before executing
 jQuery(document).ready(function() {
-  
+  /*
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
   	
   });
 
-}
+}*/
 });
